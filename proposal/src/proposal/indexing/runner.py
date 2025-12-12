1b0a0d95439183291b75fbb42a7dfe6921be5c03
Sanchay Kumar <dsanchaykumar@gmail.com>
2025-12-12 23:43:00 +0530

    Storing extracted documents data in chromadb

--- proposal/src/proposal/indexing/runner.py
+++ proposal/src/proposal/indexing/runner.py
@@ -1,9 +1,11 @@
 import os
 import logging
+import chromadb
 from typing import List
 from dotenv import load_dotenv
 from langchain_community.docstore.document import Document
 from langchain_core.runnables import RunnablePassthrough
+from langchain_community.vectorstores.chroma import Chroma
 
 from proposal.indexing.parser import ProposalParser
 from proposal.core.logging_setup import setup_logging
@@ -16,6 +18,7 @@ from proposal.indexing.sections.problem_statement import ProblemStatement
 from proposal.indexing.sections.approach import Approach
 from proposal.indexing.sections.deliverable import Deliverable
 from proposal.indexing.sections.pricing_and_timeline import PricingAndTimeline
+from proposal.core.embeddings import bge_embedding
 
 
 logger = logging.getLogger(__name__)
@@ -49,6 +52,25 @@ def main():
         )
 
     documents = chain.invoke(documents)
+
+    if 'proposal_document' in documents:
+        documents = documents['proposal_document']
+    
+
+    for doc_idx, doc in enumerate(documents):
+        if not doc.metadata.get('section_type', ''):
+            documents[doc_idx].metadata['section_type'] = '<no_section>'
+
+
+    chromadb_client = chromadb.Client()
+    vectorstore = Chroma.from_documents(
+        documents=documents,
+        embedding=bge_embedding,
+        client=chromadb_client,
+        collection_name=os.getenv('business_proposal_data'),
+        persist_directory=os.getenv('chorma_vectorstore/')
+    )
+    vectorstore.persist()
 
 
 if __name__ == '__main__':
